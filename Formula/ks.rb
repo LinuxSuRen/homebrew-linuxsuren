e491eabc66f5456c975d4a9d3d8333e693cecce7
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-09 17:19:10 +0800

    Brew formula update for ks version v0.0.8

--- Formula/ks.rb
+++ Formula/ks.rb
@@ -2,20 +2,20 @@
 class Ks < Formula
   desc "ks is a kubectl wrapper for Kubesphere"
   homepage "https://github.com/linuxsuren/ks"
-  version "0.0.7"
+  version "0.0.8"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/LinuxSuRen/ks/releases/download/v0.0.7/kubectl-ks-darwin-amd64.tar.gz"
-    sha256 "e80d98aaf9da7055ec652897f32fc55f7820e62148643d06095f30bb1e18afd7"
+    url "https://github.com/LinuxSuRen/ks/releases/download/v0.0.8/ks-darwin-amd64.tar.gz"
+    sha256 "60c17e7a26c7a3bd8e69fb5b1a3d21eccc6addc451f653f4bd4b1165a08e13e4"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/LinuxSuRen/ks/releases/download/v0.0.7/kubectl-ks-linux-amd64.tar.gz"
-    sha256 "37801cb2ac1cb977728d93ac726609f733aafb20440e93a2273fe723d663002d"
+    url "https://github.com/LinuxSuRen/ks/releases/download/v0.0.8/ks-linux-amd64.tar.gz"
+    sha256 "9966b2b48de15a27b639d24e7bc6ef497c66c8a425ecadfb49277dfe28ce2ec0"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/LinuxSuRen/ks/releases/download/v0.0.7/kubectl-ks-linux-arm64.tar.gz"
-    sha256 "fb013e2156716c6528fda7b8f96f428828cb094f450d2cdf1c9f741b4ff95b5f"
+    url "https://github.com/LinuxSuRen/ks/releases/download/v0.0.8/ks-linux-arm64.tar.gz"
+    sha256 "1965766070171e62cfdd1fe0f0be588bb603a6199026d29c4e38957f0d680e79"
   end
 
   depends_on "vim" => :optional
