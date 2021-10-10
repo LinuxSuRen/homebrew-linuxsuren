3f8b26294ec10a41b2d79ab30293c151024ab807
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-10 20:09:16 +0800

    Brew formula update for http-downloader version v0.0.44

--- Formula/hd.rb
+++ Formula/hd.rb
@@ -5,28 +5,28 @@
 class Hd < Formula
   desc "HTTP download tool"
   homepage "https://github.com/linuxsuren/http-downloader"
-  version "0.0.43"
+  version "0.0.44"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/LinuxSuRen/http-downloader/releases/download/v0.0.43/hd-darwin-amd64.tar.gz"
-      sha256 "7f1a4f4ec7e7564b70d84f7e81d7615939a40b8482debe3d82db4d78118ab2a3"
+      url "https://github.com/LinuxSuRen/http-downloader/releases/download/v0.0.44/hd-darwin-amd64.tar.gz"
+      sha256 "8009da38b940a18296adff4af3ec971df1540102e60dc8142ee625ce3c9d2515"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/LinuxSuRen/http-downloader/releases/download/v0.0.43/hd-darwin-arm64.tar.gz"
-      sha256 "2a36ad50c59b78bf89f417c9880da6973c4b03d11de6f872d8f82f1cedc2b87f"
+      url "https://github.com/LinuxSuRen/http-downloader/releases/download/v0.0.44/hd-darwin-arm64.tar.gz"
+      sha256 "f09df6c7b0c2fef638ee5c0ec0e2d4a8c57a8e9d86a041c7e54ac4b4991b6d59"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/LinuxSuRen/http-downloader/releases/download/v0.0.43/hd-linux-amd64.tar.gz"
-      sha256 "bdd215992e220a3789ff7dc808a0844927e8bd71ba4897d1ee7cece45222d40b"
+      url "https://github.com/LinuxSuRen/http-downloader/releases/download/v0.0.44/hd-linux-amd64.tar.gz"
+      sha256 "31716489be21ea27b52862e3e68b719b46a725152a7c667971eb304a475c99aa"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/LinuxSuRen/http-downloader/releases/download/v0.0.43/hd-linux-arm64.tar.gz"
-      sha256 "3379881f22a8613cbfe9cce82fdcaf89e0c13ebd36641dde8e4e4ef639ea5e0c"
+      url "https://github.com/LinuxSuRen/http-downloader/releases/download/v0.0.44/hd-linux-arm64.tar.gz"
+      sha256 "47f9a6755c0408778afd73833e4a07676b4c10e635b8d03f01a2ae273fbca2ed"
     end
   end
 
